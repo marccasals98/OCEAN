8eea1b109b20ebc82b8d6702a99ae0c9902bca13
Marc casals <marccasals@10-192-28-87client.eduroam.upc.edu>
2023-05-02 14:31:11 +0200

    first modification of the dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -4,11 +4,19 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 class BlueFinLib(Dataset):
-    def __init__(self):
+    def __init__(self, species, wav_name, pickle_path, img_dir):
         super().__init__()
+        df = pd.read_pickle(pickle_path)
+        self.species = list(df.species_labels)
+        self.wav_name = list(df['original name of wav file']) 
+        self.img_dir = img_dir 
+        #self.specie = df[df.iloc[:,1]==self.wav_name].species.values[0]
+
+
     def __len__(self):
-        # TO-DO
-        return 1
+        return len(self.species_labels)
+
     def __getitem__(self, index):
-        # TO-DO
-        return 1
+        img_path = os.path.join(self.img_dir, self.wav_name[index]) # arreglar: s'hauran de borrar les cometes 'audiofile.wav'
+        label = species_labels[index]
+        return image, label
