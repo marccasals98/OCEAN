9e52390e81426ae6b43cc88e70ed8cff94cb1f86
marccasals98 <marc.casals8@gmail.com>
2023-08-03 19:31:35 +0200

    added the split file that will make the partition of the dataset

--- src/split.py
+++ src/split.py
@@ -1,79 +1,38 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
 import os
-from collections import defaultdict
-import random
+import shutil
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
+# Define directories and proportions
+source_directory = '/home/usuaris/veussd/DATABASES/Ocean/DATASETS/Original_20230802/data'
+train_directory = '/home/usuaris/veussd/DATABASES/Ocean/DATASETS/Original_20230802_Train/data'
+valid_directory = '/home/usuaris/veussd/DATABASES/Ocean/DATASETS/Original_20230802_Valid/data'
+test_directory = '/home/usuaris/veussd/DATABASES/Ocean/DATASETS/Original_20230802_Test/data'
 
-class Split():
-    """
-    Split the dataset in train and test.
+# Create directories
+os.makedirs(train_directory, exist_ok=True)
+os.makedirs(valid_directory, exist_ok=True)
+os.makedirs(test_directory, exist_ok=True)
 
-    Attributes:
-    -----------
-    src_path: str
-        The original path with the files you want to use.
-    
-    train_path: str
-        The path where the train files will be saved.
+# List all files and shuffle them
+all_files = os.listdir(source_directory)
+all_files = shuffle(all_files)
 
-    test_path: str
-        The path where the test files will be saved.    
-    
-    test_size: float
-        The proportion of the dataset to include in the test split.
-    
-    random_state: int
-        Controls the shuffling applied to the data before applying the split.
+# Extract class labels
+class_labels = [file_name.split('_')[2] for file_name in all_files]
 
-    Methods:
-    --------
-    get_class_labels()
-        Get class labels and their corresponding file paths.
-    
-    split()
-        Split the dataset in train and test.
+# Perform stratified split
+train_files, test_files, _, _ = train_test_split(all_files, class_labels, test_size=0.2, stratify=class_labels, random_state=42)
+train_files, valid_files, _, _ = train_test_split(train_files, [file_name.split('_')[0] for file_name in train_files], test_size=0.25, stratify=[file_name.split('_')[0] for file_name in train_files], random_state=42)
 
-    """
-    def __init__(self, src_path, train_path, test_path, test_size=0.2, random_state=None):
-        self.src_path = src_path
-        self.train_path = train_path
-        self.test_path = test_path
-        self.test_size = test_size
-        self.random_state = random_state
-    
-    def get_class_labels(self):
-        # Get class labels and their corresponding file paths
-        class_files = defaultdict(list)
-        for class_name in os.listdir(self.src_path):
-            class_path = os.path.join(self.src_path, class_name)
-            if os.path.isdir(class_path):
-                class_files[class_name] = [os.path.join(class_path, file_name) for file_name in os.listdir(class_path)]
-        return class_files
+# Function to move files
+def move_files(file_list, destination_directory):
+    for file_name in file_list:
+        source_path = os.path.join(source_directory, file_name)
+        destination_path = os.path.join(destination_directory, file_name)
+        shutil.move(source_path, destination_path)
 
-    def split(self):
-        """
-        Split the dataset in train and test.
-        """
-        class_files = self.get_class_labels(self)
-        
-        # Shuffle the data within each class
-        for class_name, file_paths in class_files.items():
-            random.shuffle(file_paths)
-
-        # Perform the stratified split
-        train_files = []
-        test_files = []
-        for class_name, file_paths in class_files.items():
-            train_data, test_data = train_test_split(file_paths,
-                                                    test_size=self.test_size,
-                                                    random_state=self.random_state,
-                                                    stratify=[class_name]*len(file_paths))
-            train_files.extend(train_data)
-            test_files.extend(test_data)
-
-        # Create the training and testing folders
-        os.makedirs(self.train_folder, exist_ok=True)
-        os.makedirs(self.test_folder, exist_ok=True)
-
-
+# Move files to respective directories
+move_files(train_files, train_directory)
+move_files(valid_files, valid_directory)
+move_files(test_files, test_directory)
