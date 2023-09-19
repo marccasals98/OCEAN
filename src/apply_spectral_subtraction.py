d83cbe3074e30ab39ca836c214814505dca07bde
Jaume Prats Cristià <jaume.prats.cristia@veue02.tsc.upc.edu>
2023-09-19 11:44:50 +0200

    changes to ecompass noise

--- src/apply_spectral_subtraction.py
+++ src/apply_spectral_subtraction.py
@@ -20,15 +20,12 @@ def main(params):
 
     new_srate = 250 # we downsample to 250Hz
 
+    total_subtractions = 0 # We set a counter to know how many correct subtractions we have done.
+
     for filename in tqdm(os.listdir(input_path)):
 
         wav_path = os.path.join(input_path, filename)
         sample_rate, raw_sig = wavfile.read(wav_path) # open file
-
-        #RESAMPLING:
-        resampling_factor = new_srate / sample_rate
-        samples = int(resampling_factor * len(raw_sig))
-        resampled_sig = signal.resample(raw_sig, samples).astype(np.int16)
 
         # removing noise if spectral_subtraction is set
         window_sec = 0.256 # window used for spectral subtraction
@@ -36,10 +33,23 @@ def main(params):
 
 
         if spectral_subtraction_prob >= (random.randint(0,10)/10):
-            cleaned_sig = ss.reduce_noise(resampled_sig, resampled_sig, winsize=window_samples, add_noisy_phase=True)
+
+            try:
+                wav_noise_path = os.path.join(noise_path, filename) # Load noise estimation of the signal:
+                sample_rate, raw_noise_sig = wavfile.read(wav_noise_path) # open noise estimator.
+                # Spectral subtraction with noise estimation.
+                if len(raw_noise_sig) != 0:
+                    total_subtractions += 1 # increase the counter by one.
+                    cleaned_sig = ss.reduce_noise(raw_sig, raw_noise_sig, winsize=window_samples, add_noisy_phase=True)
+                else:
+                    cleaned_sig = raw_sig    
+            except FileNotFoundError:
+                cleaned_sig = raw_sig
         else:
             cleaned_sig = raw_sig
         wavfile.write(os.path.join(output_path, 'Spectral_Subtraction_' +filename), sample_rate, cleaned_sig)
+    
+    print("The number of correct spectral subtractions that we have done is: ", total_subtractions)
     
 
 if __name__=="__main__":
